d74930fb7c489ef9c24d48631989efc3228d6183
Mohamed Mohamed Ibrahim <mohamedgawad600@gmail.com>
2026-03-04 09:30:24 +0200

    Adding get_best_guess.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import math, json
 import pandas as pd
 from collections import Counter
+import numpy as np
+
+from feedback import pattern_str_to_code
 
 N_ITERS = 6
 
@@ -35,8 +38,7 @@ def pattern_code(guess, target):
 
 def precompute_pattern_matrix():
     if pattern_matrix_file.exists():
-        pattern_matrix = pd.read_csv(pattern_matrix_file)
-        return pattern_matrix
+        return pd.read_csv(pattern_matrix_file)
 
     with open(guess_file_path, "r") as file:
         guesses = json.load(file)
@@ -62,8 +64,28 @@ def precompute_pattern_matrix():
     return df
 
 
-def get_best_guess():
-    pass
+def get_best_guess(pattern_matrix):
+    print(pattern_matrix)
+    print()
+    best_guess = ""
+    best_information_gain = float()
+
+    for guess in pattern_matrix.columns:
+        patterns = pattern_matrix.loc[:, guess]
+        codes = [pattern_str_to_code(pattern) for pattern in patterns]
+        cnts = Counter(codes)
+
+        distribution = []
+        for cnt in cnts.values():
+            distribution.append(cnt / len(codes))
+        information_gain = 0
+        for prob in distribution:
+            information_gain += prob * math.log2(prob)
+        information_gain *= -1
+        if information_gain > best_information_gain:
+            best_guess = guess
+            best_information_gain = information_gain
+    return best_guess, best_information_gain
 
 
 def print_iter(h_w, h_y, best_word):
@@ -87,4 +109,5 @@ def main():
 if __name__ == "__main__":
     # main()
     pattern_matrix = precompute_pattern_matrix()
+    print(get_best_guess(pattern_matrix))
     # print(pattern_code("ALLEY", "APPLE"))
