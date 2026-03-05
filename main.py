dd6f685d1f5f9df89c0f73077847d8ff814feee3
Mohamed Mohamed Ibrahim <mohamedgawad600@gmail.com>
2026-03-05 07:40:40 +0200

    Fixing get_entropy.

--- main.py
+++ main.py
@@ -157,7 +157,9 @@ def get_entropy(pattern_matrix, guess, feedback, targets):
     cnts = Counter(codes)
     print(cnts[feedback])
     prob = cnts[feedback] / len(codes)
-    entropy = -1 * prob * math.log2(prob) if prob < 0 else 0
+    prob = -1 * prob * math.log2(prob)
+    entropy = prob if prob > 0 else 0
+    print()
     return entropy
 
 
